0010134f89b1beaf8d1c6663be2dcfe6cbe05430
Filipe Hanniel <filipehanniel@gmail.com>
2023-07-25 19:48:11 -0300

    teste exercicio04

--- src/exercicio04.java
+++ src/exercicio04.java
@@ -1,27 +1,37 @@
 import java.util.Scanner;
 
 public class exercicio04 {
-    
+
     public static void main(String[] args) {
-        
+
         char[][] text = new char[1000][80];
+        String enterText;
+        int countWhite = 0, countA = 0, l = 0;
 
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Digite um texto com 80000 caracteres:");
+        enterText = sc.nextLine();
         for (int i = 0; i < 1000; i++) {
             for (int j = 0; j < 80; j++) {
-                text[i][j] = sc.next().charAt(0);
-                System.out.println(text[i][j]);
-                if (j == 10) {
-                    break;
-                }
-            }
-            if (i == 1) {
-                break;
+              //  if (i > 0) {                
+              //      l = 80 + l;
+              //  }
+              //  text[i][j] = enterText.charAt(l);
             }
         }
-
-
+        /*
+         * for (int i = 0; i < 1000; i++) {
+         * for (int j = 0; j < 80; j++) {
+         * if (text[i][j] == ' ') {
+         * countWhite += 1;
+         * } else if (text[i][j] == 'a') {
+         * countA += 1;
+         * }
+         * }
+         * }
+         */
+        System.out.println("Quantidade de espaços em branco = " + countWhite);
+        System.out.println("Quantidade de letras 'a' = " + countA);
 
         sc.close();
 
